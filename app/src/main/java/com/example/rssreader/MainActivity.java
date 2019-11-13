c17050671e8dc76318a6e60db680b7b914b8fe00
gwyrwch <gwyrwch@gmail.com>
2019-11-13 19:27:27 +0300

    xml downloading is now working with button click

--- app/src/main/java/com/example/rssreader/MainActivity.java
+++ app/src/main/java/com/example/rssreader/MainActivity.java
@@ -14,6 +14,8 @@ import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.rssreader.Models.RSSItem;
 import com.example.rssreader.ViewModels.ItemViewModel;
@@ -49,10 +51,38 @@ public class MainActivity extends AppCompatActivity implements DownloadCallback 
         Log.d(DEBUG_TAG, String.valueOf(isOnline()));
 
 
-        mNetworkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), "https://www.google.com");
+        mNetworkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), "https://news.tut.by/rss/press.rss");
+        startDownload();
 
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i % 50);
+        }
+
+        finishDownloading();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // When the user clicks FETCH, fetch the first 500 characters of
+            // raw HTML from www.google.com.
+            case R.id.fetch_action:
+                startDownload();
+                return true;
+            // Clear the text and cancel download.
+            case R.id.clear_action:
+                finishDownloading();
+                System.out.println("clear to empty text");
+                return true;
+        }
+        return false;
+    }
 
     public boolean isOnline() {
         ConnectivityManager connMgr = (ConnectivityManager)
@@ -60,6 +90,8 @@ public class MainActivity extends AppCompatActivity implements DownloadCallback 
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnected());
     }
+
+
 
     private void startDownload() {
         if (!mDownloading && mNetworkFragment != null) {
@@ -72,10 +104,10 @@ public class MainActivity extends AppCompatActivity implements DownloadCallback 
     @Override
     public void updateFromDownload(String result) {
         if (result != null) {
-            Log.d("RESULT", result);
+            Log.d("RESULT1", result);
 //            mDataText.setText(result);
         } else {
-            Log.d("RESULT", "connection error");
+            Log.d("RESULT1", "connection error");
 //            mDataText.setText(getString("connection error"));
         }
     }
@@ -101,15 +133,19 @@ public class MainActivity extends AppCompatActivity implements DownloadCallback 
         switch(progressCode) {
             // You can add UI behavior for progress updates here.
             case Progress.ERROR:
+                Log.d("error", "error in onProgressUpdate");
                 break;
             case Progress.CONNECT_SUCCESS:
+                Log.d("connect success", "connect success in onProgressUpdate");
                 break;
             case Progress.GET_INPUT_STREAM_SUCCESS:
+                Log.d("get input stream success", "get input stream success in onProgressUpdate");
                 break;
             case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
                 Log.d("percentComplete", "" + percentComplete + "%");
                 break;
             case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                Log.d("process input stream success", "process input stream success in onProgressUpdate");
                 break;
         }
     }
